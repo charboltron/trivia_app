a6c15dced62a48708d27ad76fb8b3c5b6e2674ce
charboltron <boltch@pdx.edu>
2019-10-18 14:11:30 -0700

    delete crap, change trivia_first_try

--- trivia_first_try.js
+++ trivia_first_try.js
@@ -12,29 +12,29 @@ let trivia = JSON.parse(rawdata);
 let count = 0;
 let score = 0;
 
-console.log("\n\nWELCOME TO THE GAME!!!!");
 var recursiveAsyncReadLine = function () {
-  console.log("\nQuestion "+ (count+1) + "/10:\n");
-  var rand = trivia[Math.floor(Math.random() * trivia.length)];
-  console.log(rand["question"],"\nA.",rand["A"],"\nB.",rand["B"],"\nC.",rand["C"],"\nD.",rand["D"]);
-  rl.question('Make a guess: ', (answer) => {
-      //answer = answer.toUpperCase;
-      console.log(`You chose: ${rand[answer]}`);
-      if(answer === rand["answer"]){
-          console.log("You got it!");
-          score++;
-      }
-      else{
-          console.log("Wrong! The answer was: " +rand["answer"]);
-      }
-      rl.close;
-    recursiveAsyncReadLine(); //Calling this function again to ask new question
-    count++;
-  });
   if(count == 10){
     console.log("\n\nGame over! Your score: "+score);
     process.exit();
   }
+  console.log("\nQuestion "+ (count+1) + "/10:\n");
+  var rand = trivia[Math.floor(Math.random() * trivia.length)];
+  console.log(rand["question"],"\nA.",rand["A"],"\nB.",rand["B"],"\nC.",rand["C"],"\nD.",rand["D"]);
+  rl.question('Make a guess: ', (guess) => {
+      //guess = guess.toUpperCase;
+      console.log(`You chose: ${guess}. ${rand[guess]}`);
+      if(guess === rand["answer"]){
+          console.log("You got it!");
+          score++;
+      }
+      else{
+          console.log(`Wrong! The answer was: ${rand["answer"]}. ${rand[rand["answer"]]}`);
+      }
+      rl.close;
+    count++;
+    recursiveAsyncReadLine(); //Calling this function again to ask new question
+    
+  });
 };
-
+console.log("\n\nWELCOME TO THE GAME!!!!");
 recursiveAsyncReadLine();
