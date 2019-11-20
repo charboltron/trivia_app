72e523a334ff06b36f14b37121ed6714acc9c963
charboltron <boltch@pdx.edu>
2019-11-19 23:37:47 -0800

    chaz finish quick_game mode

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 
 var path = require('path');
-//app.use(express.static(__dirname+'./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join('./public')));
 
-// app.get('/', (req, res)=>{
-//     res.sendFile('./public/index.html', { root: __dirname });
-//   })
+app.get('/quick_game', function (req, res) {
+    res.sendFile(__dirname+'/public/quick_game.html');
+  })
 
+app.get('/game_setup_solo', function (req, res) {
+     //TODO: res.sendFile(__dirname+'/public/game_setup_solo.html');
+     res.send('This is where the game setup will be (SOLO MODE)')
+})
+
+app.get('/game_setup_friend', function (req, res) {
+    //TODO: res.sendFile(__dirname+'/public/game_setup_friend.html');
+    res.send('This is where the game setup will be (FRIEND MODE)')
+})
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('listening on port 3000'));
+app.listen(PORT, () => console.log('listening on port X000'));
