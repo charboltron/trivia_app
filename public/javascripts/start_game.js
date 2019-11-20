ddce734cef9dc71316b6cec1dd882aa92d639f86
charboltron <boltch@pdx.edu>
2019-11-20 01:05:36 -0800

    added timer, crude scoring system, made comments for code sections

--- public/javascripts/start_game.js
+++ public/javascripts/start_game.js
@@ -1,101 +1,142 @@
 'use strict';
-var count;
+
+//---------------------------------------
+//Global variables needed by functions
+//---------------------------------------
+
+//TODO: decide if some kind of object would be better to contain these variables. 
+
+var timerId;
+var timer;
+var times_up;
+
+var question_count;
 var score;
 var current_question;
 var question_set = [];
+
+//Bools
 var guessed;
-var started =false;
+var started = false;
 var sound = false;
+
+//API Variables
 var trivia;
 var url = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
-function shuffle(array) {
-  var m = array.length, t, i;
+//---------------------------------------
+//Onclick Function calls from HTML
+//---------------------------------------
 
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-
-  return array;
-}
-  
-
-function load(){
+function load(){ //Hides the buttons until start game is pressed. 
     document.getElementById("guess_buttons").style.visibility = 'hidden';
     document.getElementById("current_question").style.visibility = 'hidden';
     document.getElementById("next_question").style.visibility = 'hidden';
 }
 
-async function start_game(){
+async function start_game(){ //Function is async because of API call
 
+    score = 0;
+    //Call API 
+    //TODO: update method with passed in url or other idea. 
+    //A good idea might be to move the API call to the load() function?
     let response = await fetch(url);
-    console.log(response);
+    //console.log(response);
     let data = await response.json();
     trivia = data.results;
-    console.log(trivia);
-    count = 0;
+    //console.log(trivia);
+    
+    //Reset variables
+    question_count = 0;
     score = 0;
     started = true;
     guessed = false;
-    // for(let i = 0; i < 10; i++){
-    //   //console.log("\nQuestion "+ (i+1) + "/10:\n");
-    //   var rand = trivia[Math.floor(Math.random() * trivia.length)];
-    //   question_set.push(rand);
-    //   //console.log(rand["question"],"\nA.",rand["A"],"\nB.",rand["B"],"\nC.",rand["C"],"\nD.",rand["D"]);
-    // }
     
+    //Show the buttons on the screen 
     document.getElementById("guess_buttons").style.visibility = 'visible';
     document.getElementById("current_question").style.visibility = 'visible';
     document.getElementById("next_question").style.visibility = 'visible';
+    
     display_next_question();
   }
 
 function display_next_question(){
-    question_set = [];
+    
+    //Timer
+    clearInterval(timerId);
+    times_up = false;
+    start_timer();
+    
+    //End of game logic
     guessed = false;
     if(!started){return;}
-    if(count === 10){
+    if(question_count === 10){
+        clearInterval(timerId);
         document.getElementById("display_question_number").innerHTML = ``;
+        document.getElementById("category").innerHTML = ``;
+        document.getElementById("difficulty").innerHTML = ``;
+        document.getElementById("timer").innerHTML = ``;
+        document.getElementById("guess_buttons").style.visibility = 'hidden';
         document.getElementById("display_question").innerHTML = "GAME OVER! Start a new game?";
+        document.getElementById("display_feedback").innerHTML = ``;
+        document.getElementById("display_score").innerHTML = `Final Game Score: ${score}`;
         started = false;
         return;
     }
+
+    //Clear feedback 
     document.getElementById("display_feedback").innerHTML = ``;
-    console.log("HERE");
-    current_question = trivia[count];
-    console.log(current_question.question);
+
+    //Prepare the question set and the current question
+    question_set = [];
+    current_question = trivia[question_count];
+    //console.log(current_question.question);
+    console.log(current_question.difficulty);
+    
     let question = current_question.question;
     let ans = current_question.correct_answer;
     let wrong_1 = current_question.incorrect_answers[0];
     let wrong_2 = current_question.incorrect_answers[1];
     let wrong_3 = current_question.incorrect_answers[2];
+
+    let category = current_question.category;
+    let difficulty = current_question.difficulty
+    difficulty = difficulty.charAt(0).toUpperCase() + difficulty.slice(1)
+    
     question_set.push(ans, wrong_1, wrong_2, wrong_3);
     question_set = shuffle(question_set);
+    
+    //Put the question/choices on the buttons
     document.getElementById("A_button_text").innerHTML = question_set[0];
     document.getElementById("B_button_text").innerHTML = question_set[1];
     document.getElementById("C_button_text").innerHTML = question_set[2];
     document.getElementById("D_button_text").innerHTML = question_set[3];
-    document.getElementById("display_question_number").innerHTML = `QUESTION ${count+1}: `;
+
+    //Display text for questions
+    document.getElementById("display_question_number").innerHTML = `QUESTION ${question_count+1}: `;
+    document.getElementById("category").innerHTML = `${category} `;
+    document.getElementById("difficulty").innerHTML = `Rating: ${difficulty} `;
     document.getElementById("display_question").innerHTML = question;
-    //document.getElementById("display_choices").innerHTML = "A. "+ans_a+"  B. "+ans_b+"  C. "+ans_c+"  D. "+ans_d;
-    count++;
+    
+    question_count++;
 }
 
 function guess_answer(button_id){
-    console.log("guessed: "+guessed);
-    console.log("started: "+started);
+    
+    //Timer
+    clearInterval(timerId);
+    
+    //console.log("guessed: "+guessed);
+    //console.log("started: "+started);
+    
+    //Unusual circumstances
     if(guessed || !started){
         return;
     }
+
     guessed = true;
+    
+    //Handle button clicked logic 
     var guess_letter;
     switch(button_id){
         case "guess_A":
@@ -113,12 +154,37 @@ function guess_answer(button_id){
         default:
             guess_letter="None"; //??
     }
-    console.log(`${guess_letter} button was pressed`);
+    //console.log(`${guess_letter} button was pressed`);
     //console.log(`You chose: ${guess_letter}. ${current_question[guess_letter]}`);
+    
+    //Determine correct or not, award points, give feedback
     if(guess_letter === current_question.correct_answer){
-        console.log("You got it!");
-        score++;
-        document.getElementById("display_feedback").innerHTML = `You got it!`;
+        //console.log("You got it!");
+        if(!times_up){
+          var bonus; 
+          switch(current_question.difficulty){
+            case "easy":
+                bonus = 0;
+                break;
+                case "medium":
+                bonus = 2;
+                break;
+                case "hard":
+                bonus = 4;
+                break;
+            default:
+                bonus = 0;
+        }
+          var points = Math.ceil(timer/2);
+          console.log(points);
+          if(points === 0){
+            points++;
+          }
+          score+= points+bonus;
+        }
+        let plural = (points === 1? '' : 's');
+        //TODO: Give different Feedback based on point value? 
+        document.getElementById("display_feedback").innerHTML = `You got it! That's ${points} point${plural}! Bonus: ${bonus}`;
         if(sound){
             var chime = document.getElementById("audio_correct"); 
             chime.play(); 
@@ -133,9 +199,53 @@ function guess_answer(button_id){
         }
         //console.log(`Wrong! The answer was: ${current_question["answer"]}. ${current_question[current_question["answer"]]}`);
     }
+
+    //Update score
     document.getElementById("display_score").innerHTML = `Score: ${score}`;
+}
+
+
+//---------------------------------------
+//Functions for Widgets
+//---------------------------------------
+
+function start_timer() {
+  
+  timer = 10;
+  timerId = setInterval(function(){ 
+    document.getElementById("timer").innerText = "TIME: "+timer; 
+    timer-=1;
+    if(timer === -1){
+      times_up = true;
+      clearInterval(timerId);
+      //display_next_question(); Should the time running out autoload next question? 
+    }
+  },1000);
+    
 }
 
 function toggle_sound(){
     sound = !sound;
+}
+
+
+//---------------------------------------
+//Miscellaneous functions
+//---------------------------------------
+
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
 }
