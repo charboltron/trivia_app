215b549a6db0ab2aa52f68d0dd94ae4bf8eb921e
Chad Tolleson <tolleson@pdx.edu>
2019-11-21 16:13:54 -0800

    added postgresql integration stuff

---
+++ public/javascripts/chad_postgreSQL_test_2.js
@@ -0,0 +1,21 @@
+
+// These are the instructions from the Heroku website: 
+// https://devcenter.heroku.com/articles/heroku-postgresql#designating-a-primary-database
+// DOES NOT WORK, NEEDS MODERN ERROR CATCHING
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
---
+++ public/javascripts/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+uery('SELECT $1 AS value', 123)
+//   .then(function (data) {
+//     console.log('DATA:', data.value)
+//   })
+//   .catch(function (error) {
+//     console.log('ERROR:', error)
+//   })
