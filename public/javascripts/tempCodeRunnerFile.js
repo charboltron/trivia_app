242551200ae60000ace254a3b7f0c897bb0d2fc2
Chad Tolleson <tolleson@pdx.edu>
2019-11-23 14:37:10 -0800

    removed old testing files

--- public/javascripts/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-pool.query('SELECT $1 AS value', 123)
-//   .then(function (data) {
-//     console.log('DATA:', data.value)
-//   })
-//   .catch(function (error) {
-//     console.log('ERROR:', error)
-//   })
