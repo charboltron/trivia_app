f8cd3f5e51148334df512f677e635841d4906c37
Chad Tolleson <tolleson@pdx.edu>
2019-11-25 13:40:54 -0800

    additional changes made during group coding session in library basement

--- public/javascripts/quick_game_default.js
+++ public/javascripts/quick_game_default.js
@@ -13,6 +13,6 @@ async function set_default_game() {
     } else{
         localStorage.setItem('apiJSON', JSON.stringify(trivia));
         localStorage.setItem('questionCount', 10);
-        document.getElementById("quick_game_hidden_button").click();
+        document.getElementById("hidden_quick_game_button").click();
     }
 }
