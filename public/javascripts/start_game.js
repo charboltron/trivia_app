db02c3511d8f00b065841300fd583fdd30429d2d
charboltron <boltch@pdx.edu>
2019-12-02 13:17:50 -0800

    added colors and styles to timer and gameplay, moved things around, etc

--- public/javascripts/start_game.js
+++ public/javascripts/start_game.js
@@ -32,20 +32,19 @@ var user_signed_in;
 
 function load(){ //Hides the buttons until start game is pressed. 
     document.getElementById("guess_buttons").style.visibility = 'hidden';
-    document.getElementById("current_question").style.visibility = 'hidden';
     document.getElementById("next_question").style.visibility = 'hidden';
     trivia = JSON.parse(localStorage.getItem('apiJSON'));    
-    console.log(`trivia: ${JSON.stringify(trivia)}`);
+    //console.log(`trivia: ${JSON.stringify(trivia)}`);
     total_questions = localStorage.getItem('questionCount');
-    console.log(`total questions: ${total_questions}`);
+    total_questions = Number(total_questions);
+    //console.log(`total_questions type: ${typeof(total_questions)}`);
+    //console.log(`total_questions: ${total_questions}`);
     user_signed_in = localStorage.getItem('userSignedIn');
     console.log(`user signed in is: ${user_signed_in}`);
     // alert(`trivia: ${JSON.stringify(trivia)}\n\ntotal questions: ${total_questions}\n\nuser signed in is: ${user_signed_in}`);
 }
 
 async function start_game(){ //Function is async because of API call
-
-    score = 0;
     
     //Reset variables
     question_count = 0;
@@ -56,14 +55,14 @@ async function start_game(){ //Function is async because of API call
     //Show the buttons on the screen and hide start button
     document.getElementById("start").style.visibility = 'hidden';
     document.getElementById("guess_buttons").style.visibility = 'visible';
-    document.getElementById("current_question").style.visibility = 'visible';
     
     display_next_question();
   }
 
 function display_next_question(){
     
-    document.getElementById("next_question").style.visibility = 'hidden';
+    display_question_buttons();
+    
     //Timer
     clearInterval(timerId);
     times_up = false;
@@ -72,18 +71,9 @@ function display_next_question(){
     //End of game logic
     guessed = false;
     if(!started){return;}
-    console.log(question_count);
     if(question_count == total_questions){
-        clearInterval(timerId);
-        document.getElementById("display_question_number").innerHTML = ``;
-        document.getElementById("category").innerHTML = ``;
-        document.getElementById("difficulty").innerHTML = ``;
-        document.getElementById("timer").innerHTML = ``;
-        document.getElementById("guess_buttons").style.visibility = 'hidden';
-        document.getElementById("next_question").style.visibility = 'hidden';
-        document.getElementById("display_question").innerHTML = "GAME OVER! Start a new game?";
-        document.getElementById("display_feedback").innerHTML = ``;
-        document.getElementById("display_score").innerHTML = `Final Game Score: ${score}`;
+
+        display_end_game(); 
         started = false;
         return;
     }
@@ -91,35 +81,11 @@ function display_next_question(){
     //Clear feedback 
     document.getElementById("display_feedback").innerHTML = ``;
 
-    //Prepare the question set and the current question
-    answer_options = [];
-    current_question = trivia[question_count];
-    
-    let question = current_question.question;
-    let ans = current_question.correct_answer;
-    let wrong_1 = current_question.incorrect_answers[0];
-    let wrong_2 = current_question.incorrect_answers[1];
-    let wrong_3 = current_question.incorrect_answers[2];
+    //Generate next question 
+    prepare_questions();
+    put_questions_on_buttons();
+    display_text_for_questions();
 
-    let category = current_question.category;
-    let difficulty = current_question.difficulty
-    difficulty = difficulty.charAt(0).toUpperCase() + difficulty.slice(1)
-    
-    answer_options.push(ans, wrong_1, wrong_2, wrong_3);
-    answer_options = shuffle(answer_options);
-    
-    //Put the question/choices on the buttons
-    document.getElementById("A_button_text").innerHTML = answer_options[0];
-    document.getElementById("B_button_text").innerHTML = answer_options[1];
-    document.getElementById("C_button_text").innerHTML = answer_options[2];
-    document.getElementById("D_button_text").innerHTML = answer_options[3];
-
-    //Display text for questions
-    document.getElementById("display_question_number").innerHTML = `QUESTION ${question_count+1}: `;
-    document.getElementById("category").innerHTML = `${category} `;
-    document.getElementById("difficulty").innerHTML = `Rating: ${difficulty} `;
-    document.getElementById("display_question").innerHTML = question;
-    
     question_count++;
 }
 
@@ -144,13 +110,13 @@ function guess_answer(button_id){
         case "guess_A":
             guess_letter = answer_options[0];
             break;
-            case "guess_B":
+        case "guess_B":
             guess_letter = answer_options[1];
             break;
-            case "guess_C":
+        case "guess_C":
             guess_letter = answer_options[2];
             break;
-            case "guess_D":
+        case "guess_D":
             guess_letter = answer_options[3];
             break;
         default:
@@ -161,6 +127,7 @@ function guess_answer(button_id){
     
     //Determine correct or not, award points, give feedback
     if(guess_letter === current_question.correct_answer){
+        document.getElementById(button_id).style.backgroundColor = '#7FFF00';
         //console.log("You got it!");
         if(!times_up){
           var bonus; 
@@ -182,7 +149,7 @@ function guess_answer(button_id){
             bonus_flag = true;
           }
           var points = Math.ceil(timer/2);
-          console.log(points);
+          //console.log(points);
           if(points === 0){
             points++;
           }
@@ -198,6 +165,7 @@ function guess_answer(button_id){
         }
     }
     else{
+        document.getElementById(button_id).style.backgroundColor = '#DC143C';
         var penalty = 0;
         if(timer > 12){
           penalty = 500;
@@ -238,13 +206,13 @@ function guess_answer(button_id){
     }
 
     //Update scores
-    if(penal_flag){
-      document.getElementById("display_penalty").innerHTML = `PENALTY:\n${penal_total}`;
-    }
-    if(bonus_flag){
-      document.getElementById("display_bonus").innerHTML = `BONUS:\n${bonus}`;
-    }
-    document.getElementById("display_score").innerHTML = `SCORE:\n${score}`;
+    // if(penal_flag){
+    //   document.getElementById("display_penalty").innerHTML = `PENALTY:\n${penal_total}`;
+    // }
+    // if(bonus_flag){
+    //   document.getElementById("display_bonus").innerHTML = `BONUS:\n${bonus}`;
+    // }
+    document.getElementById("display_score").innerHTML = `SCORE: ${score}`;
 
     document.getElementById("next_question").style.visibility = 'visible';
 }
@@ -254,12 +222,23 @@ function guess_answer(button_id){
 //Functions for Widgets
 //---------------------------------------
 
+var time_colors = ['1F0', '3F0', '5F0', '7F0', '9F0', 'BF0', 'DF0', 'FF0', 'FD0', 'FB0', 
+                   'F90', 'F70', 'F50', 'F30', 'F10', 'F00'];
+time_colors.reverse();                   
+
 function start_timer() {
   
   timer = 15;
   timerId = setInterval(function(){ 
-    document.getElementById("timer").innerText = "TIME: "+timer; 
+    document.getElementById("timer").style.backgroundColor = '#'+time_colors[timer]; 
+    document.getElementById("timer").innerText = timer; 
     timer-=1;
+    if(timer === 2){   
+      if(sound){
+        var chime = document.getElementById("final_3_seconds"); 
+        chime.play(); 
+    }
+  }
     if(timer === -1){
       times_up = true;
       clearInterval(timerId);
@@ -281,7 +260,7 @@ function toggle_sound(){
 
 
 //---------------------------------------
-//Miscellaneous functions
+//Next question triggered functions
 //---------------------------------------
 
 function shuffle(array) {
@@ -299,4 +278,68 @@ function shuffle(array) {
     array[i] = t;
   }
   return array;
+}
+
+function display_question_buttons(){
+    
+    document.getElementById("next_question").style.visibility = 'hidden';
+    document.getElementById("guess_A").style.backgroundColor = '#FFF';
+    document.getElementById("guess_B").style.backgroundColor = '#FFF';
+    document.getElementById("guess_C").style.backgroundColor = '#FFF';
+    document.getElementById("guess_D").style.backgroundColor = '#FFF';
+
+}
+
+function display_text_for_questions(){
+
+  let question = current_question.question;
+  let category = current_question.category;
+  let difficulty = current_question.difficulty;
+  difficulty = difficulty.charAt(0).toUpperCase() + difficulty.slice(1);
+
+  document.getElementById("display_question_number").innerHTML = `QUESTION ${question_count+1} of ${total_questions} `;
+  document.getElementById("category").innerHTML = `${category} -  ${difficulty} `;
+  document.getElementById("display_score").innerHTML = `SCORE: ${score} `;
+  document.getElementById("display_question").innerHTML = question;
+
+}
+
+function display_end_game(){
+
+  console.log("end of game reached.");
+  clearInterval(timerId);
+  document.getElementById("display_question_number").innerHTML = ``;
+  document.getElementById("category").innerHTML = ``;
+  document.getElementById("difficulty").innerHTML = ``;
+  document.getElementById("timer").innerHTML = ``;
+  document.getElementById("guess_buttons").style.visibility = 'hidden';
+  document.getElementById("next_question").style.visibility = 'hidden';
+  document.getElementById("start").style.visibility = 'visible';
+  document.getElementById("display_question").innerHTML = "GAME OVER! Start a new game?";
+  document.getElementById("display_feedback").innerHTML = ``;
+  document.getElementById("display_score").innerHTML = `Final Game Score: ${score}`;
+}
+
+function prepare_questions(){
+  
+  answer_options = [];
+  current_question = trivia[question_count];
+
+  let ans = current_question.correct_answer;
+  let wrong_1 = current_question.incorrect_answers[0];
+  let wrong_2 = current_question.incorrect_answers[1];
+  let wrong_3 = current_question.incorrect_answers[2];
+
+  answer_options.push(ans, wrong_1, wrong_2, wrong_3);
+  answer_options = shuffle(answer_options);
+  
+}
+
+function put_questions_on_buttons(){
+  
+  document.getElementById("A_button_text").innerHTML = answer_options[0];
+  document.getElementById("B_button_text").innerHTML = answer_options[1];
+  document.getElementById("C_button_text").innerHTML = answer_options[2];
+  document.getElementById("D_button_text").innerHTML = answer_options[3];
+
 }
